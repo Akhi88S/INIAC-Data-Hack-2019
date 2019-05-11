c94c890bd75c91c250a32dd488b360bc81d55d1c
Akhi88 <39363939+Akhi88@users.noreply.github.com>
2019-05-11 23:16:40 +0530

    Firstq Commit

--- app.js
+++ app.js
@@ -217,7 +217,8 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
-
-app.listen("3000", function(){
-	console.log("Blog running on server 3000");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
