aec219feff2dde757c5718257028792a35fe6ee5
Akhi88 <39363939+Akhi88@users.noreply.github.com>
2019-05-11 23:23:43 +0530

    make it better1

--- app.js
+++ app.js
@@ -217,8 +217,8 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
